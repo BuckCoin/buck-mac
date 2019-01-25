f76b95415bbeb1391ab5289e9735b1db7f2e9528
unknown <BuckCoinOrg@gmail.com>
2019-01-25 16:40:44 -0500

    Version 1.1.0.5 Updates

--- src/chainparams.h
+++ src/chainparams.h
@@ -81,6 +81,7 @@ public:
     std::string GetFoundersRewardAddressAtIndex(int i) const;
     /** Enforce coinbase consensus rule in regtest mode */
     void SetRegTestCoinbaseMustBeProtected() { consensus.fCoinbaseMustBeProtected = true; }
+	int GetNewTimeRule() const { return newTimeRule; }
 protected:
     CChainParams() {}
 
@@ -106,6 +107,7 @@ protected:
     bool fTestnetToBeDeprecatedFieldRPC = false;
     Checkpoints::CCheckpointData checkpointData;
     std::vector<std::string> vFoundersRewardAddress;
+	int newTimeRule;
 };
 
 /**
